5fd3dc9d145ff00a9d6a515884dc3b0ff6bc42b4
pipipi397 <kappi_182_mk@icloud.com>
2025-01-10 21:01:35 +0900

    [修正] ARDrawingViewとARDrawingControllerRepresentableの統合修正 - setupARSessionメソッドを追加してARセッションを適切に初期化 - CollisionCastHitでの位置取得にpositionプロパティを使用 - パンジェスチャで手書き機能を実装（球体の配置） - ARDrawingViewからARDrawingControllerRepresentableを正しく参照可能に修正

---
+++ BlendAR2/Views/AR/ARDrawingControllerRepresentable.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct ARDrawingControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ARDrawingViewController {
+        return ARDrawingViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: ARDrawingViewController, context: Context) {}
+
+    class ARDrawingViewController: UIViewController {
+        var arView: ARView!
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            arView = ARView(frame: view.bounds)
+            view.addSubview(arView)
+
+            setupARSession()
+            setupDrawing()
+        }
+
+        private func setupDrawing() {
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+            arView.addGestureRecognizer(panGesture)
+        }
+
+        @objc private func handlePan(_ sender: UIPanGestureRecognizer) {
+            let location = sender.location(in: arView)
+            switch sender.state {
+            case .began, .changed:
+                if let hitTestResult = arView.hitTest(location).first {
+                    let sphere = ModelEntity(mesh: .generateSphere(radius: 0.01))
+                    sphere.transform.translation = hitTestResult.worldTransform.translation
+                    let anchor = AnchorEntity()
+                    anchor.addChild(sphere)
+                    arView.scene.addAnchor(anchor)
+                }
+            default:
+                break
+            }
+        }
+
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            arView.session.pause()
+        }
+    }
+}
